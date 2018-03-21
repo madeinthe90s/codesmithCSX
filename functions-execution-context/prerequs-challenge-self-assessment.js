c6cb655ecb2e0ee097563c2cea1b11ba704b41f7
madeinthe90s <mike.montero1993@gmail.com>
2018-03-21 22:47:29 +0700

    precourse-assesment

---
+++ functions-execution-context/prerequs-challenge-self-assessment.js
@@ -0,0 +1,164 @@
+
+
+
+// Challenge 1
+let num = 5;
+if (num > 2){
+ num =  num * 2
+}
+
+
+// uncomment the below line to check your work
+// console.log(num) // -> should print 10
+/*
+// Challenge 2
+let newNum = 1;
+
+if (newNum < 2){
+ newNum =  newNum * 2
+}
+*/
+
+
+// uncomment the below line to check your work
+ //console.log(newNum) // -> should print 2;
+
+// Challenge 3
+/*
+ if(num % 2 == 0)
+          console.log(true);
+        else{
+          console.log(false);
+        }
+*/
+/*
+// Challenge 4
+let checkMe = [1, 2, 3];
+
+ if(checkMe.length > 5)
+          console.log(true);
+        else{
+          console.log(false);
+        }
+*/
+
+/*
+// Challenge 5
+let isDivisibleBy4 = 24;
+
+ if(isDivisibleBy4 % 4 == 0)
+          console.log(true);
+        else{
+          console.log(false);
+        }
+*/
+/*
+
+// Challenge 6
+let count = 2;
+let count2 = 3;
+
+while (count < 10) {
+  count++;
+  while(count2 < 15)
+    count2++
+}
+*/
+// uncomment the below line to check your work
+// console.log('count: ' + count + ', count2: ' + count2) // -> should print 10 and 15
+
+// Challenge 7
+
+var addThis;
+/*
+for (let addThis = 0; addThis < 10; addThis++) {
+  console.log(addThis);
+}
+*/
+// Challenge 8
+/*
+let countDown = 10;
+
+for (countDown; countDown > 0; countDown--) {
+  console.log(countDown);
+}
+*/
+// uncomment the below line to check your work
+// console.log(countDown) // -> should print 0;
+
+// Challenge 9
+
+ 
+      
+// uncomment the below line to check your work
+ //console.log(increaseByTwo) // -> should print [3, 4, 5, 6, 7];
+
+// Challenge 10
+
+// uncomment the below line to check your work
+// console.log(increaseByTwo) // -> should print [3, 4, 50, 60, 70];
+
+/*
+// Challenge 11
+let fillMe = [];
+
+for (let i = 0; i < 11; i++) {
+          fillMe.push(i)
+}
+*/
+
+// uncomment the below line to check your work 
+//console.log(fillMe) // -> should print [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+/*
+// Challenge 12
+let modifiedFillMe = [];
+
+for (let i = 10; i > -1; i--) {
+         if( i % 2 == 0 ){
+           modifiedFillMe.push(i)
+         }
+}
+*/
+// uncomment the below line to check your work
+ //console.log(modifiedFillMe) // -> should print [10, 8, 6, 4, 2, 0]
+/*
+// Challenge 13
+let checkObj = {
+  oddNum: 1,
+  evenNum: 2,
+  foundNum: 5,
+  randomNum: 18
+};
+
+for (let i = checkObj; i < 1; i++) {
+         if ('foundNum' in checkObj == true){
+           console.log('yes')
+         }
+}
+*/
+// Challenge 14
+let objToArray = [];
+
+
+// Challenge 15
+
+// Challenge 16
+let nestedArr = [];
+
+// Challenge 17
+
+// Challenge 18
+
+// Challenge 19
+let possibleIterable = [4, 3, 9, 6, 23];
+
+// Challenge 20
+let total = 0;
+let sumMe = {
+  hello: 'there',
+  you: 8,
+  are: 7,
+  almost: '10',
+  done: '!'
+};
+
